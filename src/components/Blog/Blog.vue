482c98da64a5159f6c5f6bf352e0e32d08b5fddf
AbhiY98 <abhishekyadav88846@gmail.com>
2020-04-21 12:27:20 +0530

    Added basic editor

--- src/components/Blog/Blog.vue
+++ src/components/Blog/Blog.vue
@@ -1,20 +1,62 @@
 <template>
-  <div class="editor">
-  </div>
+<div class="editor">
+  <span>Enter Title:</span>
+  <input v-model="title" class="ma-1 input pl-1 pr-1">
+  <br><br> 
+  <quill-editor
+    ref="myTextEditor"
+    v-model="content"
+    class="editor-main"
+  >
+  </quill-editor>
+  <br>
+  <v-btn @click="save" class="ma-1">Save</v-btn>
+  <v-btn @click="preview" class="ma-1">Preview</v-btn>
+</div>
 </template>
 
 <script>
+import hljs from 'highlight.js';
+import { quillEditor } from 'vue-quill-editor';
+
+import 'highlight.js/styles/tomorrow.css';
+
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
 
 export default {
+  name: 'editor',
 
   components: {
+    quillEditor
   },
 
   data: () => ({
-    content: 'Hello World',
+    content: '<h1>Enter Content Here<h1>',
+    title: 'test'
   }),
 
   methods: {
+    save() {
+      console.log(this.content);
+    },
+    preview() {
+      sessionStorage.setItem('preview-item', this.content);
+      window.open('/viewBlog');
+    }
+  },
+
+  computed: {
+    editor() {
+      return this.$refs.myTextEditor.quill;
+    },
+    contentCode() {
+      return hljs.highlightAuto(this.content).value;
+    },
+  },
+
+  mounted() {
+    // console.log(this.editor);
   }
 }
 </script>
@@ -23,6 +65,14 @@ export default {
 
 .editor {
   padding: 4rem;
+}
+
+.editor-main {
+  background-color: grey;
+}
+
+.input {
+  border-style: solid;
 }
 
 </style>
--- src/components/Blog/ViewBlog.vue
+++ src/components/Blog/ViewBlog.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
-    FK
+  <div class="content-preview ql-snow">
+    <div class="ql-editor content" v-html="content"></div>
   </div> 
 </template>
 
 <script>
-export default {
+import 'quill/dist/quill.core.css';
+import 'quill/dist/quill.snow.css';
 
+export default {
+  created() {
+    this.content = sessionStorage.getItem('preview-item');
+    console.log(this.content);
+  }
 }
 </script>
 
 <style>
+
+.content-preview {
+  padding: 4rem;
+}
+
+.content{
+  border-style: solid;
+  padding: 2rem;
+  box-shadow: 0.2rem;
+}
 
 </style>
